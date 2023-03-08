f036117a252d90af1b1108216297d79f1ee36964
dianGrigorov <diangrigorov@gmail.com>
2023-03-08 10:09:08 +0200

    Create 08.TrafficJam.cs

---
+++ 01.StacksAndQueues/08.TrafficJam/08.TrafficJam.cs
@@ -0,0 +1,31 @@
+﻿
+int n = int.Parse(Console.ReadLine());
+
+Queue<string> cars = new Queue<string>();
+int count = 0;
+string input;
+
+while ((input = Console.ReadLine()) != "end")
+{
+
+    if (input == "green")
+    {
+        for (int i = 0; i < n; i++)
+        {
+            if (cars.Count == 0)
+            {
+                break;
+            }
+            else
+            {
+                Console.WriteLine($"{cars.Dequeue()} passed!");
+                count++;
+            }
+        }
+    }
+    else
+    {
+        cars.Enqueue(input);
+    }
+}
+Console.WriteLine($"{count} cars passed the crossroads.");
