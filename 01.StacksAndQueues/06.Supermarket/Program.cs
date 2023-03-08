e6644c1d4bb829ffa301c6e05a87664e3508a14c
dianGrigorov <diangrigorov@gmail.com>
2023-03-08 09:33:26 +0200

    Update Program.cs

--- 01.StacksAndQueues/06.Supermarket/Program.cs
+++ 01.StacksAndQueues/06.Supermarket/Program.cs
@@ -1,1 +1,21 @@
 ﻿
+string input = Console.ReadLine();
+
+Queue<string> customers = new Queue<string>();
+
+while (input != "End")
+{
+	if (input != "Paid")
+	{
+        customers.Enqueue(input);
+    }
+	else
+	{
+		while (customers.Count != 0)
+		{
+            Console.WriteLine(customers.Dequeue());
+        }
+	}
+	input = Console.ReadLine();
+}
+Console.WriteLine($"{customers.Count} people remaining.");
