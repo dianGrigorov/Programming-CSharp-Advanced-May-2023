b1417fcc04f1b8038a7418e88852f6575aeaa823
dianGrigorov <diangrigorov@gmail.com>
2023-03-15 09:12:01 +0200

    a

---
+++ 01.StacksAndQueuesExercise/01.BasicsStacksOperation/01.BasicsStacksOperation.cs
@@ -0,0 +1,39 @@
+﻿
+int[] ints = Console.ReadLine()
+    .Split()
+    .Select(int.Parse)
+    .ToArray();
+int n = ints[0];
+int popedElement = ints[1];
+int containElement = ints[2];
+
+int[] elements = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+Stack<int> stack = new Stack<int>();
+
+for (int i = 0; i < n; i++)
+{
+    stack.Push(elements[i]);
+}
+
+for (int i = 0; i < popedElement; i++)
+{
+    if (stack.Count > 0)
+    {
+        stack.Pop();
+    }
+   
+}
+
+if (stack.Contains(containElement))
+{
+    Console.WriteLine("true");
+}
+else if (stack.Count == 0)
+{
+    Console.WriteLine("0");
+}
+else
+{
+    Console.WriteLine(stack.Min()); 
+}
--- 01.StacksAndQueuesExercise/02.BasicQueueOperations/Program.cs
+++ 01.StacksAndQueuesExercise/02.BasicQueueOperations/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+int[] comArg = Console.ReadLine()
+    .Split()
+    .Select(int.Parse)
+    .ToArray();
+int[] numbers = Console.ReadLine()
+    .Split()
+    .Select(int.Parse)
+    .ToArray();
+int numbersToDequeue = comArg[1];
+int searchedNumber = comArg[2];
+Queue<int> queue = new Queue<int>(numbers);
+
+for (int i = 0; i < numbersToDequeue; i++)
+{
+    if (queue.Count > 0)
+    {
+        queue.Dequeue();
+    }
+}
+if (queue.Contains(searchedNumber))
+{
+    Console.WriteLine("true");
+}
+else if (queue.Count == 0)
+{
+    Console.WriteLine(0);
+}
+else
+{
+    Console.WriteLine(queue.Min());
+}
+
