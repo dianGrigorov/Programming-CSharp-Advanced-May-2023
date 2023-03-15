94c583a3fcd7e33f9e730189d6a639425344bd75
dianGrigorov <diangrigorov@gmail.com>
2023-03-15 09:57:02 +0200

    a

---
+++ 01.StacksAndQueuesExercise/03.MaxAndMInElement/Program.cs
@@ -0,0 +1,34 @@
+﻿
+int n = int.Parse(Console.ReadLine());
+
+Stack<int> stack = new Stack<int>();
+
+for (int i = 0; i < n; i++)
+{
+	int[] commands = Console.ReadLine()
+		.Split()
+		.Select(int.Parse)
+		.ToArray();
+	int curComm = commands[0];
+	if (curComm == 1)
+	{
+		int num = commands[1];
+		stack.Push(num);
+	}
+	if (curComm == 2)
+	{
+		if (stack.Count > 0)
+		{
+            stack.Pop();
+        }
+	}
+	if (curComm == 3)
+	{
+        Console.WriteLine(stack.Max());
+    }
+	if (curComm == 4)
+	{
+        Console.WriteLine(stack.Min());
+    }
+}
+Console.WriteLine(string.Join(", ", stack));
