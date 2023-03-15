6b427b3cd06ccc92f13a6650e57343b967a034a3
dianGrigorov <diangrigorov@gmail.com>
2023-03-15 13:26:00 +0200

    a

---
+++ 01.StacksAndQueuesExercise/05.FashionBotique/Program.cs
@@ -0,0 +1,25 @@
+﻿
+int[] clothes = Console.ReadLine()
+    .Split()
+    .Select(int.Parse)
+    .ToArray();
+int capacity = int.Parse(Console.ReadLine());
+Stack<int> clothesStack = new Stack<int>(clothes);
+int racks = 1;
+int currRack = capacity;
+while (clothesStack.Count > 0)
+{
+    int currCloth = clothesStack.Peek();
+    if (currRack - currCloth >= 0)
+    {
+        currRack -= currCloth;
+        
+        clothesStack.Pop();
+    }
+    else
+    {
+        currRack = capacity;
+        racks++;
+    }
+}
+Console.WriteLine(racks);
---
+++ 01.StacksAndQueuesExercise/06.SongsQueue/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Linq;
+
+string[] songs = Console.ReadLine().Split(", ");
+
+Queue<string> songsQueue = new Queue<string>(songs);
+
+while (true)
+{
+    string[] commandArg = Console.ReadLine().Split();
+    string currCom = commandArg[0];
+
+    if (currCom == "Play")
+    {
+        if (songsQueue.Count > 0 )
+        {
+            songsQueue.Dequeue();
+        }
+        else
+        {
+            Console.WriteLine("No more songs!");
+            break;
+        }
+    }
+    else if (currCom == "Add")
+    {
+        string song = string.Join(" ", commandArg.Skip(1));
+        if (songsQueue.Contains(song))
+        {
+            Console.WriteLine($"{song} is already contained!");
+        }
+        else
+        {
+            songsQueue.Enqueue(song);
+        }
+    }
+    else if (currCom == "Show")
+    {
+        Console.WriteLine(string.Join(", ", songsQueue));
+    }
+    
+}
