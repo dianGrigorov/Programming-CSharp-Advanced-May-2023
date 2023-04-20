307b394d4bb3ff8e42225ba2d0e9c1d880df2e4b
dianGrigorov <diangrigorov@gmail.com>
2023-04-20 12:12:50 +0300

    .

---
+++ 02.MultidementionalArrays/03.PrimaryDaigonal/Program.cs
@@ -0,0 +1,20 @@
+﻿int size = int.Parse(Console.ReadLine());
+int[,] matrix = new int[size, size];
+int diagonalSum = 0;
+
+for (int row = 0; row < size; row++)
+{
+    int[] value = Console.ReadLine().Split(' ')
+        .Select(int.Parse)
+        .ToArray();
+    for (int col = 0; col < size; col++)
+    {
+        matrix[row, col] = value[col];
+    }
+}
+
+for (int row = 0; row < matrix.GetLength(0); row++)
+{
+    diagonalSum += matrix[row, row];
+}
+Console.WriteLine(diagonalSum);
