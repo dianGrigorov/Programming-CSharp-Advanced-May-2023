96f5866695858b8ac4fb7e4b44f369eb326f2a9c
dianGrigorov <diangrigorov@gmail.com>
2023-04-23 17:10:47 +0300

    .

---
+++ 02.MultidimentionalArraysExersises/04.MatrixShuffling/Program.cs
@@ -0,0 +1,63 @@
+﻿int[] dimentions = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToArray();
+int rows = dimentions[0];
+int cols = dimentions[1];
+string[,] matrix = new string[rows, cols];
+for (int row = 0; row < rows; row++)
+{
+    string[] value = Console.ReadLine()
+        .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+       
+    for (int col = 0; col < cols; col++)
+    {
+        matrix[row, col] = value[col];
+    }
+}
+
+string command;
+
+while ((command = Console.ReadLine().ToLower()) != "end")
+{
+    string[] tokens = command
+        .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+    if (IsValidCommand(tokens))
+    {
+        string tempValue = matrix[int.Parse(tokens[1]), int.Parse(tokens[2])];
+        matrix[int.Parse(tokens[1]), int.Parse(tokens[2])] = matrix[int.Parse(tokens[3]), int.Parse(tokens[4])];
+        matrix[int.Parse(tokens[3]), int.Parse(tokens[4])] = tempValue;
+
+        PrintMatrix();
+    }
+    else
+    {
+        Console.WriteLine("Invalid input!");
+    }
+
+
+}
+
+bool IsValidCommand(string[] tokens)
+{
+    return
+        tokens[0] == "swap" &&
+        tokens.Length == 5 &&
+        int.Parse(tokens[1]) >= 0 && int.Parse(tokens[1]) < rows &&
+        int.Parse(tokens[2]) >= 0 && int.Parse(tokens[2]) < cols &&
+        int.Parse(tokens[3]) >= 0 && int.Parse(tokens[3]) < rows &&
+        int.Parse(tokens[4]) >= 0 && int.Parse(tokens[4]) < cols;
+
+}
+
+void PrintMatrix()
+{
+    for (int row = 0; row < rows; row++)
+    {
+        for (int col = 0; col < cols; col++)
+        {
+            Console.Write(matrix[row, col] + " ");
+        }
+        Console.WriteLine();
+    }
+}
