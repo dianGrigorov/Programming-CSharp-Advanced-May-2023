4eca6d2332d2417076c33d7337ffb3f4045f3c7b
dianGrigorov <diangrigorov@gmail.com>
2023-04-23 15:49:36 +0300

    .

---
+++ 02.MultidimentionalArraysExersises/02.SquaresInMatrix/Program.cs
@@ -0,0 +1,34 @@
+﻿int[] dimentions = Console.ReadLine()
+    .Split()
+    .Select(int.Parse)
+    .ToArray();
+int rows = dimentions[0];
+int cols = dimentions[1];
+char[,] matrix = new char[rows, cols];
+int count = 0;
+for (int row = 0; row < rows; row++)
+{
+    char[] chars = Console.ReadLine()
+        .Split()
+        .Select(char.Parse)
+        .ToArray();
+    for (int col = 0; col < cols; col++)
+    {
+        matrix[row, col] = chars[col];
+       
+    }
+}
+for (int row = 0; row < rows - 1; row++)
+{
+    for (int col = 0; col < cols - 1; col++)
+    {
+        if (matrix[row, col] == matrix[row, col + 1]
+           && matrix[row, col] == matrix[row + 1, col]
+           && matrix[row, col] == matrix[row + 1, col + 1])
+        {
+            count++;
+        }
+    }
+}
+Console.WriteLine(count);
+
