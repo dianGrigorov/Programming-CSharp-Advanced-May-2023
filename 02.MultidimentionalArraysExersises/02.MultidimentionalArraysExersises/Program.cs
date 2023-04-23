e5f83814a6e087b11168dce8c292c087aa247c84
dianGrigorov <diangrigorov@gmail.com>
2023-04-23 15:30:10 +0300

    .

---
+++ 02.MultidimentionalArraysExersises/02.MultidimentionalArraysExersises/Program.cs
@@ -0,0 +1,21 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+int[,] matrix = new int[n, n];
+int primarySum = 0;
+int secondarySum = 0;
+for (int row = 0; row < matrix.GetLength(0); row++)
+{
+    int[] value = Console.ReadLine().Split()
+        .Select(int.Parse)
+        .ToArray();
+    for (int col = 0; col < matrix.GetLength(1); col++)
+    {
+        matrix[row, col] = value[col];
+    }
+}
+for (int row = 0; row < matrix.GetLength(0); row++)
+{
+    primarySum += matrix[row, row];
+    secondarySum += matrix[row, matrix.GetLength(0) - 1 - row];
+}
+Console.WriteLine(Math.Abs(primarySum - secondarySum));
