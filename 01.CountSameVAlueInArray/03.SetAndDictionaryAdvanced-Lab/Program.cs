cefc5b49d265ea26eb2082ed6ab650755294e6ab
dianGrigorov <diangrigorov@gmail.com>
2023-05-20 12:04:32 +0300

    .

---
+++ 01.CountSameVAlueInArray/03.SetAndDictionaryAdvanced-Lab/Program.cs
@@ -0,0 +1,18 @@
+﻿double[] numbers = Console.ReadLine()
+    .Split()
+    .Select(double.Parse)
+    .ToArray();
+
+Dictionary<double, int> countValues = new Dictionary<double, int>();
+for (int i = 0; i < numbers.Length; i++)
+{
+    if (!countValues.ContainsKey(numbers[i]))
+    {
+        countValues.Add(numbers[i], 0);
+    }
+    countValues[numbers[i]]++;
+}
+foreach (var item in countValues)
+{
+    Console.WriteLine($"{item.Key} - {item.Value} times");
+}
