818d2e45ed02d3ca560440e676d521f1b33368c2
dianGrigorov <diangrigorov@gmail.com>
2023-05-20 12:20:51 +0300

    .

---
+++ 03.SetsAndDictionriesAdvanced-Lab/02.AverageStudentsGrades/Program.cs
@@ -0,0 +1,29 @@
+﻿int n = int.Parse(Console.ReadLine());
+Dictionary<string, List<decimal>> averageGrades = new Dictionary<string, List<decimal>>();
+
+for (int i = 0; i < n; i++)
+{
+    string[] tokens = Console.ReadLine()
+        .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+    string name = tokens[0];
+    decimal grade = decimal.Parse(tokens[1]);
+
+    if (!averageGrades.ContainsKey(name))
+    {
+        averageGrades.Add(name, new List<decimal>());
+    }
+    averageGrades[name].Add(grade);
+}
+foreach (var student in averageGrades)
+{
+    Console.Write($"{student.Key} -> ");
+
+    foreach (var grades in student.Value)
+    {
+        Console.Write($"{grades:f2} ");
+
+    }
+    Console.WriteLine($"(avg: {student.Value.Average():f2})");
+
+
+}
