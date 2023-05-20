5841497dc647f7dabc2c31b07769ed0c0680bf37
dianGrigorov <diangrigorov@gmail.com>
2023-05-20 12:27:27 +0300

    .

---
+++ 03.SetsAndDictionriesAdvanced-Lab/03.Last3Numbers/Program.cs
@@ -0,0 +1,10 @@
+﻿int[] numbers = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToArray();
+
+int[]sortedNumbers = numbers.OrderByDescending(n => n).ToArray();
+for (int i = 0; i < 3 && i < sortedNumbers.Length; i++)
+{
+    Console.Write($"{sortedNumbers[i]} ");
+}
