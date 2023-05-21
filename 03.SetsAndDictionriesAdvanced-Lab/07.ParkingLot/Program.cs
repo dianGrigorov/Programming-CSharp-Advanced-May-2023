78952e74e47865992e83ce0b220b47beee354dc6
dianGrigorov <diangrigorov@gmail.com>
2023-05-21 17:53:44 +0300

    .

---
+++ 03.SetsAndDictionriesAdvanced-Lab/07.ParkingLot/Program.cs
@@ -0,0 +1,27 @@
+﻿HashSet<string> carPlate =  new HashSet<string>();
+string command;
+while ((command = Console.ReadLine()) != "END")
+{
+    string[] commArg = command.Split(", ", StringSplitOptions.RemoveEmptyEntries);
+    string currCom = commArg[0];
+    string plate = commArg[1];
+    if (currCom == "IN")
+    {
+        carPlate.Add(plate);
+    }
+    if(currCom == "OUT")
+    {
+        carPlate.Remove(plate);
+    }
+}
+if (carPlate.Any())
+{
+    foreach (var plate in carPlate)
+    {
+        Console.WriteLine(plate);
+    }
+}
+else
+{
+    Console.WriteLine("Parking Lot is Empty");
+}
---
+++ 03.SetsAndDictionriesAdvanced-Lab/08.SoftUniParty/Program.cs
@@ -0,0 +1,40 @@
+﻿HashSet<string> invited = new HashSet<string>();
+HashSet<string> invitedVip = new HashSet<string>();
+
+string command = Console.ReadLine();
+while (true)
+{
+    if (command == "PARTY")
+    {
+        while ((command = Console.ReadLine()) != "END")
+        {
+            invited.Remove(command);
+            invitedVip.Remove(command);
+        }
+        break;
+    }
+    else
+    {
+        bool isDigit = Char.IsDigit(command[0]);
+        if (isDigit)
+        {
+            invitedVip.Add(command);
+        }
+        else
+        {
+            invited.Add(command);
+        }
+    }
+    command = Console.ReadLine();
+}
+
+Console.WriteLine(invited.Count + invitedVip.Count);
+
+foreach (var guest in invitedVip)
+{
+    Console.WriteLine(guest);
+}
+foreach (var guest in invited)
+{
+    Console.WriteLine(guest);
+}
