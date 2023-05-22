58d2cdd8f319491e9ac04092e65a423355dd8fe4
dianGrigorov <diangrigorov@gmail.com>
2023-05-22 08:52:45 +0300

    .

--- 03.SetAndDictionaryAdvanced-Exercise/05.CountSymbols/Program.cs
+++ 03.SetAndDictionaryAdvanced-Exercise/05.CountSymbols/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string input = Console.ReadLine();
+
+Dictionary<char, int> symbols = new Dictionary<char, int>();
+
+for (int i = 0; i < input.Length; i++)
+{
+    char currChar = input[i];
+
+    if (!symbols.ContainsKey(currChar))
+    {
+        symbols.Add(currChar, 0);
+    }
+    symbols[currChar]++;
+}
+
+
+foreach (var ch in symbols.OrderBy(x => x.Key))
+{
+    Console.WriteLine($"{ch.Key}: {ch.Value} times/s");
+}
