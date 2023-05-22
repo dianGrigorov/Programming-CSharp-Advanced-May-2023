af2575846c6026e0e28f16110f4e7ed010203f77
dianGrigorov <diangrigorov@gmail.com>
2023-05-22 08:45:12 +0300

    .

--- 03.SetAndDictionaryAdvanced-Exercise/03.PeriodicTable/Program.cs
+++ 03.SetAndDictionaryAdvanced-Exercise/03.PeriodicTable/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int n = int.Parse(Console.ReadLine());
+SortedSet<string> elements = new SortedSet<string>();
+
+for (int i = 0; i < n; i++)
+{
+    string[] tokens = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+    foreach (string token in tokens)
+    {
+        elements.Add(token);
+    }
+}
+
+Console.WriteLine(string.Join(" ", elements));
