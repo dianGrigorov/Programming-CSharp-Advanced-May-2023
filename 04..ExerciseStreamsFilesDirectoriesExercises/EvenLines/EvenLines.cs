a7eeb35349bd923568fb1bcdf18f18803ecc1671
dianGrigorov <diangrigorov@gmail.com>
2023-05-24 12:31:04 +0300

    .

--- 04..ExerciseStreamsFilesDirectoriesExercises/EvenLines/EvenLines.cs
+++ 04..ExerciseStreamsFilesDirectoriesExercises/EvenLines/EvenLines.cs
@@ -1,18 +1,68 @@
-﻿namespace EvenLines
+﻿namespace EvenLines;
+
+using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+public class EvenLines
 {
-    using System;
-    public class EvenLines
+    static void Main()
     {
-        static void Main()
-        {
-            string inputFilePath = @"..\..\..\text.txt";
+        string inputFilePath = @"..\..\..\text.txt";
 
-            Console.WriteLine(ProcessLines(inputFilePath));
+        Console.WriteLine(ProcessLines(inputFilePath));
+    }
+
+    public static string ProcessLines(string inputFilePath)
+    {
+        StringBuilder sb = new();
+
+        using StreamReader streamReader = new(inputFilePath);
+
+        string line = string.Empty;
+
+        int count = 0;
+
+        while (line != null)
+        {
+            line = streamReader.ReadLine();
+
+            if (count % 2 == 0)
+            {
+                string replacedSymbols = ReplaceSymbols(line);
+                string reversedWords = ReverseWords(replacedSymbols);
+                sb.AppendLine(reversedWords);
+            }
+
+            count++;
         }
 
-        public static string ProcessLines(string inputFilePath)
+        return sb.ToString().TrimEnd();
+    }
+
+    private static string ReplaceSymbols(string line)
+    {
+        StringBuilder sb = new(line);
+
+        char[] symbolsToReplace = { '-', ',', '.', '!', '?' };
+
+        foreach (var symbol in symbolsToReplace)
         {
-            throw new NotImplementedException();
+            sb = sb.Replace(symbol, '@');
         }
+
+        return sb.ToString();
+    }
+
+    private static string ReverseWords(string words)
+    {
+        string[] reversedWords = words
+            .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+            .Reverse()
+            .ToArray();
+
+        return string.Join(" ", reversedWords);
     }
 }
+
