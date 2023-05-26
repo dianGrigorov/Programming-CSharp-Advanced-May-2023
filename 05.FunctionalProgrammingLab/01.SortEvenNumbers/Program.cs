a3a9715e40e4d4bdb6861e7a28e586fb5438878c
dianGrigorov <diangrigorov@gmail.com>
2023-05-26 12:52:28 +0300

    .

---
+++ 05.FunctionalProgrammingLab/01.SortEvenNumbers/Program.cs
@@ -0,0 +1,9 @@
+﻿int[] numbers = Console.ReadLine()
+    .Split(",", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .Where(n  => n % 2 == 0)
+    .OrderBy(n => n)
+    .ToArray();
+
+string result = string.Join(", ", numbers);
+Console.WriteLine(result);
