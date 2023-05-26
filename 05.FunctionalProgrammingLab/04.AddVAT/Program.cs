098b273e58ef9347bec3eb59b3fb2770ab0190a6
dianGrigorov <diangrigorov@gmail.com>
2023-05-26 13:37:36 +0300

    .

---
+++ 05.FunctionalProgrammingLab/04.AddVAT/Program.cs
@@ -0,0 +1,11 @@
+﻿double[] tokens = Console.ReadLine()
+    .Split(", ")
+    .Select(double.Parse)
+    .ToArray();
+
+tokens = tokens.Select(x => x * 1.2).ToArray();
+//tokens.ToList().ForEach(x => Console.WriteLine($"{x *= 1.2:f2}"));
+foreach (var token in tokens)
+{
+    Console.WriteLine(token);
+}
