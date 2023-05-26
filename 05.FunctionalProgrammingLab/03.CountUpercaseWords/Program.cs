e287d0d4ac79cd441a4e302b147e964cd2fe3d15
dianGrigorov <diangrigorov@gmail.com>
2023-05-26 13:19:48 +0300

    .

---
+++ 05.FunctionalProgrammingLab/03.CountUpercaseWords/Program.cs
@@ -0,0 +1,8 @@
+﻿//Predicate<string> checker = n => n[0] == n.ToUpper()[0];
+
+List<string> tokens = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .Where(w => w[0] == w.ToUpper()[0])
+    .ToList();
+
+tokens.ForEach(w =>  Console.WriteLine(w));
