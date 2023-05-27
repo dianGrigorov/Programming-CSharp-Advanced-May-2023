d46154fec6136e97e6fe6aeb969bf1f45c7d3c64
dianGrigorov <diangrigorov@gmail.com>
2023-05-27 06:23:09 +0300

    .

---
+++ 05.FunctionalProgramingExercises/03.CustomMinFunction/Program.cs
@@ -0,0 +1,19 @@
+﻿Func<HashSet<int>, int> min = numbers =>
+{
+    int min = int.MaxValue;
+    foreach (var number in numbers)
+    {
+        if (number < min)
+        {
+            min = number;
+        }
+    }
+    return min;
+};
+
+HashSet<int> numbers = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToHashSet();
+
+Console.WriteLine(min(numbers));
