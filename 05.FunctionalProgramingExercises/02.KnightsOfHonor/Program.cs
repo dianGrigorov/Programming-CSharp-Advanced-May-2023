f9940b56c84231bb08d1479fd96ec6a60257a97a
dianGrigorov <diangrigorov@gmail.com>
2023-05-27 06:06:04 +0300

    .

---
+++ 05.FunctionalProgramingExercises/02.KnightsOfHonor/Program.cs
@@ -0,0 +1,10 @@
+﻿Action<string, string[]> printWithTitle = (title, names) =>
+{
+    foreach (var name in names)
+    {
+        Console.WriteLine($"{title} {name}");
+    }
+};
+string[] names = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+printWithTitle("Sir", names);
