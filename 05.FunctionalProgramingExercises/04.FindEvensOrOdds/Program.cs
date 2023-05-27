a127d5bd5318b4ea281ddddaae5e5cac4a4a78da
dianGrigorov <diangrigorov@gmail.com>
2023-05-27 06:55:25 +0300

    .

---
+++ 05.FunctionalProgramingExercises/04.FindEvensOrOdds/Program.cs
@@ -0,0 +1,41 @@
+﻿Func<int, int, List<int>> generateRange = (start, end) =>
+{
+    List<int> range = new List<int>();
+
+    for (int i = start; i <= end; i++)
+    {
+        range.Add(i);
+    }
+
+    return range;
+};
+
+Func<string, int, bool> evenOddMatch = (condition, number) =>
+{
+    if (condition == "even")
+    {
+        return number % 2 == 0;
+    }
+    else
+    {
+        return number % 2 != 0;
+    }
+};
+
+int[] range = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToArray();
+
+string command = Console.ReadLine();
+
+List<int> numbers = generateRange(range[0], range[1]);
+
+
+foreach (var number in numbers)
+{
+    if (evenOddMatch(command, number))
+    {
+        Console.Write($"{number} ");
+    }
+}
