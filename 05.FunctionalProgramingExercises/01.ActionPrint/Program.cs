56786a70a8a4bb77a2d1d03d0240c7b99421b886
dianGrigorov <diangrigorov@gmail.com>
2023-05-27 05:57:20 +0300

    .

---
+++ 05.FunctionalProgramingExercises/01.ActionPrint/Program.cs
@@ -0,0 +1,8 @@
+﻿Action<string[]> print = name =>
+{
+    Console.WriteLine(string.Join(Environment.NewLine, name));
+
+};
+
+string[] names = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+print(names);
