dc9440b84fadd2213761df8df54199ed71010a26
dianGrigorov <diangrigorov@gmail.com>
2023-05-28 09:59:05 +0300

    .

---
+++ 05.FunctionalProgramingExercises/06.ReverseAndExclude/Program.cs
@@ -0,0 +1,37 @@
+﻿
+Func<List<int>, List<int>> reverse = numbers =>
+{
+    List<int> result = new List<int>();
+
+    for (int i = numbers.Count - 1; i >= 0; i--)
+    {
+        result.Add(numbers[i]);
+    }
+    return result;
+};
+
+
+Func<List<int>, Predicate<int>, List<int>> exclude = (numbers, match) =>
+{
+    List<int> result = new List<int>();
+    foreach (var number in numbers)
+    {
+        if (!match(number))
+        {
+            result.Add(number);
+        }
+    }
+    return result;
+};
+List<int> numbers = Console.ReadLine()
+        .Split()
+        .Select(int.Parse)
+        .ToList();
+
+int divider = int.Parse(Console.ReadLine());
+Predicate<int> checkDivider = n => n % divider == 0;
+
+numbers = exclude(numbers, checkDivider);
+numbers = reverse(numbers);
+
+Console.WriteLine(string.Join(" ", numbers));
---
+++ 05.FunctionalProgramingExercises/07.PradicateForNames/Program.cs
@@ -0,0 +1,19 @@
+﻿
+Action<string[], Predicate<string>> print = (names, match) =>
+{
+    foreach (var name in names)
+    {
+        if (match(name))
+        {
+            Console.WriteLine(name);
+        }
+    }
+};
+
+int lenght = int.Parse(Console.ReadLine());
+
+string[] names = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+Predicate<string> matchLenght = n => n.Length <= lenght;
+print(names, matchLenght);
+//print(names, n => n.Length <= lenght);
