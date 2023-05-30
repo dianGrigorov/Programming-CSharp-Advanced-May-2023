0a0c58af022124399d013293a52cf233469e61f0
dianGrigorov <diangrigorov@gmail.com>
2023-05-30 13:12:58 +0300

    .

---
+++ 06.DefiningClasses/CarManufacturer/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace CarManufacturer;
+
+public class StartUp
+{
+    static void Main()
+    {
+        Car car = new Car()
+        {
+            Make = "VW",
+            Model = "Pasat",
+            Year = 2006,
+            FuelQuantity = 200,
+            FuelConsumption = 200,
+
+        };
+        car.Drive(2000);
+        Console.WriteLine(car.WhoAmI());
+
+        
+    }
+}
