8d8aabeab30d9bc9346d8483f36320a198bf1069
dianGrigorov <diangrigorov@gmail.com>
2023-06-01 10:43:16 +0300

    .

---
+++ 06.DefiningClassesExercises/DefiningClasses/StartUp.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace DefiningClasses;
+
+public class StartUp
+{
+    static void Main(string[] args)
+    {
+        Family family = new Family();
+
+        int count = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < count; i++)
+        {
+            string[] input = Console.ReadLine().Split(' ',StringSplitOptions.RemoveEmptyEntries);
+            Person person = new Person(input[0], int.Parse(input[1]));
+            family.AddMember(person);
+        }
+
+       Person oldest  = family.GetOldestMember();
+
+        Console.WriteLine($"{oldest.Name} {oldest.Age}");
+
+    }
+}
