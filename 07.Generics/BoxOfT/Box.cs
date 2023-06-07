6a90d1d59a51098f21d9aaa276ce15e2c7508434
dianGrigorov <diangrigorov@gmail.com>
2023-06-07 10:52:57 +0300

    .

---
+++ 07.Generics/BoxOfT/Box.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoxOfT
+{
+    public class Box<T>
+    {
+       private List<T> items = new();
+
+        public void Add(T element)
+        {
+            items.Add(element);
+        }
+
+        public T Remove()
+        {
+            T firstElement = items[items.Count -1];
+            items.RemoveAt(items.Count - 1);
+            return firstElement;
+        }
+
+        public int Count
+        {
+            get { return items.Count; }
+        }
+       
+    }
+}
---
+++ 07.Generics/GenericArrayCreator/StartUp.cs
@@ -0,0 +1,4 @@
+﻿using GenericArrayCreator;
+
+string[] strings = ArrayCreator.Create(5, "Pesho");
+int[] integers = ArrayCreator.Create(5, 2);
