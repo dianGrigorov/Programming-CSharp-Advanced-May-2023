eaf64276835536cdd1761a198193355a2c08b5a1
dianGrigorov <diangrigorov@gmail.com>
2023-06-13 13:17:33 +0300

    .

---
+++ Exams/18August2022/BaristaContest/Program.cs
@@ -0,0 +1,78 @@
+﻿Queue<int> coffee = new(Console.ReadLine()
+    .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse));
+
+Stack<int> milk = new(Console.ReadLine()
+    .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse));
+
+Dictionary<string, int> drinks = new()
+{
+    { "Cortado", 0},
+    { "Espresso", 0},
+    { "Capuccino", 0 },
+    { "Americano", 0},
+    { "Latte", 0}
+};
+
+while(coffee.Count > 0 && milk.Count > 0)
+{
+    int currCoffee = coffee.Dequeue();
+    int currMilk = milk.Pop();
+    int result = currCoffee + currMilk;
+    if (result == 50)
+    {
+        drinks["Cortado"]++;
+    }
+    else if(result == 75)
+    {
+        drinks["Espresso"]++;
+    }
+    else if (result == 100)
+    {
+        drinks["Capuccino"]++;
+    }
+    else if (result == 150)
+    {
+        drinks["Americano"]++;
+    }
+    else if (result == 200)
+    {
+        drinks["Latte"]++;
+    }
+    else
+    {
+        milk.Push(currMilk - 5);
+    }
+}
+
+if (coffee.Count == 0 && milk.Count == 0)
+{
+   
+    Console.WriteLine("Nina is going to win! She used all the coffee and milk!");
+
+}
+else
+{
+    Console.WriteLine("Nina needs to exercise more! She didn't use all the coffee and milk!");
+}
+if (coffee.Count > 0)
+{
+    Console.WriteLine($"Coffee left: {string.Join(", ", coffee)}");
+}
+else
+{
+    Console.WriteLine("Coffee left: none");
+}
+if (milk.Count > 0)
+{
+    Console.WriteLine($"Milk left: {string.Join(", ", milk)}");
+}
+else
+{
+    Console.WriteLine("Milk left: none");
+}
+foreach (var drink in drinks.OrderBy(x => x.Value).ThenByDescending(x => x.Key).Where(x => x.Value > 0))
+{
+    Console.WriteLine($"{drink.Key}: {drink.Value}");
+}
---
+++ Exams/18August2022/ConsoleApp1/Program.cs
@@ -0,0 +1,161 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = int.Parse(Console.ReadLine());
+
+            char[,] field = new char[size, size];
+            int moleRow = 0;
+            int moleCol = 0;
+            int firstSpecilRow = 0;
+            int firstSpecilCol = 0;
+            int secondSpecialRow = 0;
+            int secondSpecialCol = 0;
+            bool isFirstSpecialFound = false;
+            int points = 0;
+            for (int row = 0; row < size; row++)
+            {
+                string tokens = Console.ReadLine();
+
+                for (int col = 0; col < size; col++)
+                {
+                    field[row, col] = tokens[col];
+                    if (field[row, col] == 'M')
+                    {
+                        moleRow = row;
+                        moleCol = col;
+                    }
+                    else if (field[row, col] == 'S' && !isFirstSpecialFound)
+                    {
+                        firstSpecilRow = row;
+                        firstSpecilCol = col;
+                        isFirstSpecialFound = true;
+                    }
+                    else if (field[row, col] == 'S')
+                    {
+                        secondSpecialRow = row;
+                        secondSpecialCol = col;
+                    }
+
+                }
+            }
+            bool winTheGame = false;
+            string command;
+            while ((command = Console.ReadLine()) != "End")
+            {
+              
+                if (command == "up")
+                {
+                    Move(ref moleRow, ref moleCol, -1, 0, ref points);
+                    
+                }
+                else if (command == "down")
+                {
+                    Move(ref moleRow, ref moleCol, 1, 0,ref points);
+                   
+                }
+                else if (command == "left")
+                {
+                    Move(ref moleRow, ref moleCol, 0, -1, ref points);
+                    
+                }
+                else if (command == "right")
+                {
+                    Move(ref moleRow, ref moleCol, 0, 1, ref points);
+                    
+                }
+                if (points >= 25)
+                {
+                    winTheGame = true;
+                    break;
+                }
+            }
+
+
+            if (winTheGame)
+            {
+                Console.WriteLine("Yay! The Mole survived another game!");
+                Console.WriteLine($"The Mole managed to survive with a total of {points} points.");
+            }
+            else
+            {
+                Console.WriteLine("Too bad! The Mole lost this battle!");
+                Console.WriteLine($"The Mole lost the game with a total of {points} points.");
+            }
+
+            PrintMatrix(field);
+            bool IsInside(int row, int col)
+            {
+                return row >= 0 && row < field.GetLength(0)
+                    && col >= 0 && col < field.GetLength(1);
+
+            }
+
+            void Move(
+                ref int moleRow,
+                ref int moleCol,
+                int nextRow,
+                int nextCol,
+                ref int points)
+            {
+                if (IsInside(moleRow + nextRow, moleCol + nextCol))
+                {
+
+                    if (field[moleRow + nextRow, moleCol + nextCol] == '-')
+                    {
+                        field[moleRow, moleCol] = '-';
+                        moleRow += nextRow;
+                        moleCol += nextCol;
+                    }
+                    else if (char.IsDigit(field[moleRow + nextRow, moleCol + nextCol]))
+                    {
+                        field[moleRow, moleCol] = '-';
+                        moleRow += nextRow;
+                        moleCol += nextCol;
+                        points += int.Parse(field[moleRow, moleCol].ToString()); ;
+                    }
+                    else if (field[moleRow + nextRow, moleCol + nextCol] == 'S')
+                    {
+                        field[moleRow, moleCol] = '-';
+                        moleRow += nextRow;
+                        moleCol += nextCol;
+                        if (moleRow == firstSpecilRow && moleCol == firstSpecilCol)
+                        {
+                            moleRow = secondSpecialRow;
+                            moleCol = secondSpecialCol;
+                            field[firstSpecilRow, firstSpecilCol] = '-';
+                        }
+                        else if (moleRow == secondSpecialRow && moleCol == secondSpecialCol)
+                        {
+                            moleRow = firstSpecilRow;
+                            moleCol = firstSpecilCol;
+                            field[secondSpecialRow, secondSpecialCol] = '-';
+                        }
+                        points -= 3;
+                    }
+                    field[moleRow, moleCol] = 'M';
+                }
+                else
+                {
+                    Console.WriteLine("Don't try to escape the playing field!");
+
+                }
+            }
+            void PrintMatrix(char[,] matrix)
+            {
+                for (int row = 0; row < matrix.GetLength(0); row++)
+                {
+                    for (int col = 0; col < matrix.GetLength(1); col++)
+                    {
+                        Console.Write(matrix[row, col]);
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
